1d7668762b80a6363ae0404cba779d4c72f4f25a
nildiert <julianaga01@gmail.com>
2020-04-14 19:34:59 +0000

    [FEATURE] -concat matrixes and transpose

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -7,6 +7,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
     m1 = [x[:] for x in mat1]
     m2 = [x[:] for x in mat2]
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return m1 + m2
     else:
+        if len(mat1) != len(mat2):
+            return None
         return [m1[i] + m2[i] for i in range(len(m1))]
---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Ridin’ Bareback"""
+
+
+def mat_mul(mat1, mat2):
+    """Matrix multiplication"""
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    result = []
+
+
+    for i in range(len(mat1)):
+        result.append([])
+        for j in range(len(mat2[0])):
+            result[i].append(0)
+            for k in range(len(mat2)):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+
+    return result
