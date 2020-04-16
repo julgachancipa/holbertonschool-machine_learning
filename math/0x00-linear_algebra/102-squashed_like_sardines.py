06e660f741dc311b6e7b163a69776fbcad083ad2
nildiert <julianaga01@gmail.com>
2020-04-16 22:34:30 +0000

    [FEATURE] - dif shape mtx

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Squashed Like Sardines"""
+from functools import reduce
 
 
 def cat_matrices(mat1, mat2, axis=0):
@@ -16,23 +17,42 @@ def cat_matrices(mat1, mat2, axis=0):
     final_shape = mat1_shape[:]
     final_shape[axis] = mat1_shape[axis] + mat2_shape[axis]
 
-    """print('s1: {}, s2: {}, fs: {}'
-    .format(mat1_shape, mat2_shape, final_shape))
-    """
-    arr = sort_arr(arr1, arr2, mat1_shape,
-                   mat2_shape, final_shape, axis)
+    if mat1_shape == mat2_shape:
+        arr = sort_arr(arr1, arr2, mat1_shape,
+                       mat2_shape, axis)
+    else:
+        arr = sort_dif(arr1, arr2, mat1_shape,
+                       mat2_shape, axis)
     return reshape(arr, final_shape)
 
 
-def sort_arr(arr1, arr2, sh1, sh2, fs, ax):
+def sort_dif(arr1, arr2, sh1, sh2, ax):
+    """Sort array following the new shape"""
+    aux = []
+    n1 = 1
+    n2 = 1
+    for i in range(1, len(sh1)):
+        n1 *= sh1[i]
+        n2 *= sh2[i]
+    st1 = 0
+    st2 = 0
+    for t in range(sh1[(ax - 1)]):
+        aux += arr1[st1:(st1 + n1)]
+        aux += arr2[st2:(st2 + n2)]
+        st1 += n1
+        st2 += n2
+    return aux
+
+
+def sort_arr(arr1, arr2, sh1, sh2, ax):
     """Sort array following the new shape"""
     arr = []
     st1 = 0
     st2 = 0
 
     while st1 < len(arr1) and st2 < len(arr2):
-        arr += arr1[st1:(st1+sh1[ax])]
-        arr += arr2[st2:(st2+sh2[ax])]
+        arr += arr1[st1:(st1 + sh1[ax])]
+        arr += arr2[st2:(st2 + sh2[ax])]
         st1 += sh1[ax]
         st2 += sh2[ax]
     return arr
@@ -40,14 +60,14 @@ def sort_arr(arr1, arr2, sh1, sh2, fs, ax):
 
 def matrix_shape(matrix):
     """calculates the shape of a matrix"""
-    if not type(matrix) == list:
+    if not isinstance(matrix, list):
         return []
     return [len(matrix)] + matrix_shape(matrix[0])
 
 
 def flat_mtx(matrix):
     """Flat a matrix"""
-    if not type(matrix) == list:
+    if not isinstance(matrix, list):
         return []
 
     if len(matrix_shape(matrix)) <= 1:
@@ -69,8 +89,8 @@ def reshape(lst, shape):
     if len(shape) == 1:
         return lst
     n = reduce(mul, shape[1:])
-    return [reshape(lst[i*n:(i+1)*n], shape[1:])
-            for i in range(len(lst)//n)]
+    return [reshape(lst[i * n:(i + 1) * n], shape[1:])
+            for i in range(len(lst) // n)]
 
 
 def reduce(function, iterable, initializer=None):
