dcbbc7b5e8fb928263322577945eac884074be27
nildiert <julianaga01@gmail.com>
2020-04-24 19:53:38 +0000

    [FIX] - None case

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,7 +6,7 @@ def summation_i_squared(n):
     """Calculates the summation of i²
     in a [1, n] range"""
 
-    if n <= 1:
+    if n < 1:
         return None
     range_l = list(range(1, n+1))
     return sum(map(lambda i: i**2, range_l))
