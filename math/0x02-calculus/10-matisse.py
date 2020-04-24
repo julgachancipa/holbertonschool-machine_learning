35878ae02f5f6e475e93054220ff43f3200f139f
nildiert <julianaga01@gmail.com>
2020-04-24 23:07:58 +0000

    [FIX] - len = 1

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,11 +6,13 @@ def poly_derivative(poly):
     """calculates the derivative of a polynomial"""
     if poly == []:
         return None
+    if len(poly) == 1:
+        return [0]
     result = []
     for i in range(1, len(poly)):
         result.append(i * poly[i])
-    """
+
     if result == [0] * len(result):
         return [0]
-    """
+
     return result
