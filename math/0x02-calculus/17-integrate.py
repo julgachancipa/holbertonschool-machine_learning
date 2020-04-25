490dc6bbd72f43c1640ec1b349790c401f61eba2
nildiert <julianaga01@gmail.com>
2020-04-25 19:47:47 +0000

    [FIX] - whole numbers

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,15 +4,14 @@
 
 def poly_integral(poly, C=0):
     """calculates the integral of a polynomial"""
-    if type(poly) != list:
+    if type(poly) != list or type(C) != int:
         return None
     if poly == []:
         return None
     result = [C]
     for i in range(len(poly)):
-        if poly[i] == 0:
-            result.append(0)
-        else:
-            result.append(poly[i] / (i+1))
-
+        r = poly[i] / (i+1)
+        if r.is_integer():
+            r = int(r)
+        result.append(r)
     return result
