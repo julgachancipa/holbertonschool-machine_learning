79677b8eac3602f96535dd38718e3c50ec2a0de0
nildiert <julianaga01@gmail.com>
2020-04-25 20:17:31 +0000

    [FIX] - int coef

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,14 +6,18 @@ def poly_integral(poly, C=0):
     """calculates the integral of a polynomial"""
     if type(poly) != list or type(C) != int:
         return None
+
     if poly == []:
         return None
+
     result = [C]
+
     for i in range(len(poly)):
+        if type(poly[i]) != int:
+            return None
         r = poly[i] / (i+1)
         if r.is_integer():
             r = int(r)
         result.append(r)
-    if result == [0] * len(result):
-        return [0]
+
     return result
