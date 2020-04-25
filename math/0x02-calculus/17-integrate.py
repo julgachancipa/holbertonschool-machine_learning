1052d93287a6cbdfe85c9f2529fffa3ccc214130
nildiert <julianaga01@gmail.com>
2020-04-25 19:39:07 +0000

    [FEATURE] - Integrate

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Integrate"""
+
+
+def poly_integral(poly, C=0):
+    """calculates the integral of a polynomial"""
+    if type(poly) != list or type(C) != int:
+        return None
+    if poly == []:
+        return None
+    result = [C]
+    for i in range(len(poly)):
+        if poly[i] == 0:
+            result.append(0)
+        else:
+            result.append(poly[i] / (i+1))
+
+    if result == [0] * len(result):
+        return [0]
+
+    return result
