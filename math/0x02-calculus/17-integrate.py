fa388a2171a249b65ea8b4bb570d6ce9c5c24486
nildiert <julianaga01@gmail.com>
2020-04-25 19:54:42 +0000

    [FIX] - not numeric values

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,6 +6,9 @@ def poly_integral(poly, C=0):
     """calculates the integral of a polynomial"""
     if type(poly) != list or type(C) != int:
         return None
+    for p in poly:
+        if type(p) != int and type(p) != float:
+            return None
     if poly == []:
         return None
     result = [C]
