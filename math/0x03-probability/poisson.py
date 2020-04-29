974741a3c14bcff997a1771ba9c0358a7dd44003
nildiert <julianaga01@gmail.com>
2020-04-29 00:46:02 +0000

    [FEATURE] - Calculates the value of the CDF for a given number of successes

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
+"""Poisson distribution"""
+
+
+pi = 3.1415926536
+e = 2.7182818285
+
+def factorial(n):
+    """Finds the factorial of a given number"""
+    return 1 if (n==1 or n==0) else n * factorial(n - 1);
 
 
 class Poisson:
     """Represents a poisson distribution"""
 
     def __init__(self, data=None, lambtha=1.):
+        """Initialize Poisson"""
+        self.data = data
 
         if data is None:
             if lambtha < 0:
                 raise ValueError('lambtha must be a positive value')
             self.lambtha = float(lambtha)
+
         else:
             if type(data) != list:
                 raise TypeError('data must be a list')
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
             self.lambtha = sum(data) / len(data)
+
+
+    def pmf(self, k):
+        """Calculates the value of the PMF
+        for a given number of successes (k)"""
+
+        if type(k) != int:
+            k = int(k)
+        return (e**(-self.lambtha) * self.lambtha**k) / factorial(k)
