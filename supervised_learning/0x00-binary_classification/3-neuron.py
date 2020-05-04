0ce5cbcf59afd0b1d4071006f15a18fefc4da270
nildiert <julianaga01@gmail.com>
2020-05-04 22:13:33 +0000

    [FEATURE] - Neuron Cost

---
+++ supervised_learning/0x00-binary_classification/3-neuron.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Privatize Neuron"""
+import numpy as np
+
+
+def sigmoid(z):
+    """Calculates the sigmoid function"""
+    return 1 / (1 + np.exp(-z))
+
+
+class Neuron:
+    """defines a single neuron performing
+    binary classification"""
+
+    def __init__(self, nx):
+        """class constructor"""
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """weights vector for the neuron"""
+        return self.__W
+
+    @property
+    def b(self):
+        """bias for the neuron"""
+        return self.__b
+
+    @property
+    def A(self):
+        """activated output of the neuron (prediction)"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = sigmoid(z)
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model
+        using logistic regression"""
+        m = (Y.shape[1])
+        return -(1/m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
