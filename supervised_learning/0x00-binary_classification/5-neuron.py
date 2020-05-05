6d126fff8daed84014fde2f708d6f8678bcc8404
nildiert <julianaga01@gmail.com>
2020-05-05 16:55:35 +0000

    [FEATURE] - Neuron Gradient Descent

---
+++ supervised_learning/0x00-binary_classification/5-neuron.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""Neuron Gradient Descent"""
+import numpy as np
+
+
+def sigmoid(z):
+    """Calculates the sigmoid function"""
+    return 1 / (1 + np.exp(-z))
+
+
+class Neuron:
+    """defines a single neuron performing
+    binary classification"""
+
+    def __init__(self, nx):
+        """class constructor"""
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """weights vector for the neuron"""
+        return self.__W
+
+    @property
+    def b(self):
+        """bias for the neuron"""
+        return self.__b
+
+    @property
+    def A(self):
+        """activated output of the neuron (prediction)"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        z = np.dot(self.__W, X) + self.__b
+        self.__A = sigmoid(z)
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model
+        using logistic regression"""
+        m = (Y.shape[1])
+        return -(1/m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+
+    def evaluate(self, X, Y):
+        """Evaluates the neuron’s predictions"""
+        A = self.forward_prop(X)
+        c = self.cost(Y, A)
+        a = np.round(A)
+        return a.astype(int), c
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """Calculates one pass of gradient descent on the neuron"""
+        m = (Y.shape[1])
+        dz = A - Y
+        db = (1/m) * np.sum(dz)
+        dw = (1/m) * X * dz
+
+        self.__W = self.__W - alpha * dw.T
+        self.__b = self.__b - alpha * db
