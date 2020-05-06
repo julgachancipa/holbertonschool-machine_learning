8a0b6745deffc950f579ad331cb69f3f1ee8d6ae
nildiert <julianaga01@gmail.com>
2020-05-06 20:58:42 +0000

    [FEATURE] - defines a neural network with one hidden layer performing binary classification

---
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""NeuralNetwork"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """defines a neural network with one hidden
+    layer performing binary classification"""
+
+    def __init__(self, nx, nodes):
+        """class constructor"""
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) != int:
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros(nodes).reshape(nodes, 1)
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
