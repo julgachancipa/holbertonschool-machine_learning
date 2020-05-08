d257415d7ed2b6216d889621b7fe09696e9c806c
nildiert <julianaga01@gmail.com>
2020-05-08 20:37:47 +0000

    [FEATURE] - DeepNeuralNetwork

---
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""DeepNeuralNetwork"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """defines a deep neural network with one hidden
+    layer performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """class constructor"""
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) != list or len(layers) < 1:
+            raise TypeError('layers must be a list of positive integers')
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for i in range(self.L):
+            if type(layers[i]) != int or layers[i] < 0:
+                raise TypeError('layers must be a list of positive integers')
+            kb = "b" + str(i + 1)
+            kW = "W" + str(i + 1)
+
+            self.weights[kb] = np.zeros(layers[i]).reshape(layers[i], 1)
+            if i > 0:
+                aux = layers[i-1]
+            else:
+                aux = nx
+            self.weights[kW] = np.random.randn(layers[i], aux) * np.sqrt(2/aux)
