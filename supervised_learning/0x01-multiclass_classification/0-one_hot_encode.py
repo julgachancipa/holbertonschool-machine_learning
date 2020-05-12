824f9681343c3c8190a2ab9657adb95284ea15db
julgachancipa <julianaga01@gmail.com>
2020-05-12 11:28:18 -0500

    [FIX] - None cases

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -8,6 +8,8 @@ def one_hot_encode(Y, classes):
     vector into a one-hot matrix"""
     m = Y.shape[0]
 
+    if all(i >= classes for i in Y) or not m or type(classes) != int or classes < 2:
+        return None
     mtx = np.zeros((m, classes))
 
     for col, c_label in zip(mtx, Y):
