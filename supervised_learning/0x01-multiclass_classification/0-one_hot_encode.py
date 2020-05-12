bd2a22ae5f3f00880e04adfdffaf12bceb939750
nildiert <julianaga01@gmail.com>
2020-05-12 00:59:19 +0000

    [FEATURE] - One-Hot Encode

---
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""One-Hot Encode"""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """ that converts a numeric label
+    vector into a one-hot matrix"""
+    m = Y.shape[0]
+
+    mtx = np.zeros((m, classes))
+
+    for col, c_label in zip(mtx, Y):
+        col[c_label] = 1
+
+    return mtx.T
