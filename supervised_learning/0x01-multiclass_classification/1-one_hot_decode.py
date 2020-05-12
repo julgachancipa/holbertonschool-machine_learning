0e38c6cfd210d296f5b4f617846218fa82b261fa
julgachancipa <julianaga01@gmail.com>
2020-05-12 13:20:29 -0500

    [FIX] - Just a one in each column

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -6,4 +6,7 @@ import numpy as np
 def one_hot_decode(one_hot):
     """that converts a one-hot
     matrix into a vector of labels"""
+    for col in one_hot.T:
+        if np.sum(col) != 1:
+            return None
     return np.argmax(one_hot, axis=0)
