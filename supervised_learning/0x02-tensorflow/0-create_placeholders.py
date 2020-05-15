dd1380a1ce516b31bb83431007d434d85ac31621
julgachancipa <julianaga01@gmail.com>
2020-05-15 11:26:37 -0500

    [FEATURE] - Create placeholders

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+"""Placeholders"""
+
+
+def create_placeholders(nx, classes):
+    """returns two placeholders, x and y,
+    for the neural network"""
+    x = tf.placeholder("float", [None, nx], name='x')
+    y = tf.placeholder("float", [None, classes], name='y')
+
+    return x, y
