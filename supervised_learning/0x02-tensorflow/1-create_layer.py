e330dd2d9ec6bae86e15a3fbc0bf3b3476130bfe
julgachancipa <julianaga01@gmail.com>
2020-05-15 17:37:40 -0500

    [FEATURE] - Create layer

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 def create_layer(prev, n, activation):
     """Create Layer"""
-    output = tf.layers.Dense(name='layer', units=n, activation=activation,
-                             kernel_initializer=tf.contrib.layers.
-                             variance_scaling_initializer(mode="FAN_AVG"))
-    return output(prev)
+    layer = tf.layers.Dense(name='layer', units=n, activation=activation,
+                            kernel_initializer=tf.contrib.layers.
+                            variance_scaling_initializer(mode="FAN_AVG"))
+    return layer(prev)
