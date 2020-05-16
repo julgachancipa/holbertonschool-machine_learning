3b7c4ff7ae29d7eea512b342054c923478a6a89a
julgachancipa <julianaga01@gmail.com>
2020-05-16 13:16:53 -0500

    [FIX] - decode with argmax

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -9,5 +9,6 @@ def calculate_accuracy(y, y_pred):
     """
     calculates the accuracy of a prediction
     """
-    accuracy = tf.reduce_mean((y, y_pred))
+    correct_pred = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
     return accuracy
