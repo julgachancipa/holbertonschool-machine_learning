a2814d46713edf43f5d84e827c79e7b3a7a48b61
julgachancipa <julianaga01@gmail.com>
2020-05-16 12:45:55 -0500

    [UPDATE] - Add summary

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -10,4 +10,5 @@ def calculate_accuracy(y, y_pred):
     calculates the accuracy of a prediction
     """
     accuracy = tf.reduce_mean(tf.cast(tf.equal(y, y_pred), tf.float32))
+    tf.summary.scalar('accuracy', accuracy)
     return accuracy
