bb70f964465199ade955a4724fcf797a18599cad
julgachancipa <julianaga01@gmail.com>
2020-05-16 17:41:20 -0500

    [FIX] - Saver

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -42,6 +42,6 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
             print('\tValidation Accuracy: {}'.format(acc_v))
         sess.run(train_op, feed_dict={x: X_train, y: Y_train})
 
-    tf.train.Saver()
+    saver = tf.train.Saver()
     save_path = saver.save(sess, save_path)
     return save_path
