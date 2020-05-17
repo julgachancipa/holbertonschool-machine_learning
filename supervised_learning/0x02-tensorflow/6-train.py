aea79eed50ca22d7cab2e97181d77006fc699c7b
julgachancipa <julianaga01@gmail.com>
2020-05-16 22:49:34 -0500

    [FIX] - Saver

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -24,12 +24,12 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
     accuracy = calculate_accuracy(y, y_pred)
     train_op = create_train_op(loss, alpha)
 
-    tf.add_to_collection("x", x)
-    tf.add_to_collection("y", y)
-    tf.add_to_collection("y_pred", y_pred)
-    tf.add_to_collection("loss", loss)
-    tf.add_to_collection("accuracy", accuracy)
-    tf.add_to_collection("train_op", train_op)
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    tf.add_to_collection('y_pred', y_pred)
+    tf.add_to_collection('loss', loss)
+    tf.add_to_collection('accuracy', accuracy)
+    tf.add_to_collection('train_op', train_op)
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
@@ -48,5 +48,6 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
             print('\tValidation Cost: {}'.format(loss_v))
             print('\tValidation Accuracy: {}'.format(acc_v))
         sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+
     save_path = saver.save(sess, save_path)
     return save_path
