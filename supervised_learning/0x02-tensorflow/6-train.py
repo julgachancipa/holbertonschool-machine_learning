ff4e3586340b00130f69de2097a3d1d14b7984a7
julgachancipa <julianaga01@gmail.com>
2020-05-17 01:03:32 -0500

    [FIX] - [0]

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -50,5 +50,4 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
         if i < iterations:
             sess.run(train_op, feed_dict={x: X_train, y: Y_train})
     save_path = saver.save(sess, save_path)
-    sess.close()
     return save_path
--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -12,12 +12,14 @@ def evaluate(X, Y, save_path):
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph('{}.meta'.format(save_path))
         saver.restore(sess, save_path)
-        x = tf.get_collection('x')
-        y = tf.get_collection('y')
-        y_pred = tf.get_collection('y_pred')
-        loss = tf.get_collection('loss')
-        accuracy = tf.get_collection('accuracy')
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        loss = tf.get_collection('loss')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        train_op = tf.get_collection('train_op')[0]
 
-    y_pred_e, acc_e, loss_e = sess.run((y_pred, accuracy, loss),
-                                       feed_dict={x: X, y: Y})
+        sess.run(train_op, feed_dict={x: X, y: Y})
+        y_pred_e, acc_e, loss_e = sess.run((y_pred, accuracy, loss),
+                                           feed_dict={x: X, y: Y})
     return y_pred_e, acc_e, loss_e
