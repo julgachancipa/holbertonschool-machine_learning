7e34ef0c4497fb5c3f02cd43c0f76ee01a3cbdbb
julgachancipa <julianaga01@gmail.com>
2020-05-21 14:36:43 -0500

    [FEATURE] - learning rate tf

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -2,11 +2,12 @@
 """
 Learning Rate Decay
 """
+import numpy as np
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """
     Updates the learning rate
     """
-    alpha *= (1 / (1 + decay_rate * int(global_step / decay_step)))
+    alpha *= (1 / (1 + decay_rate * np.floor(global_step / decay_step)))
     return alpha
