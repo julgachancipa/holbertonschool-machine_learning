f34f7064609e9b493d8c3b091942f6f41d8fcce6
julgachancipa <julianaga01@gmail.com>
2020-05-21 14:29:34 -0500

    [FEATURE] - learning rate tf

---
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Learning Rate Decay Upgraded
+"""
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    Creates the training operation for a neural network
+    """
+    learning_rate = tf.train.inverse_time_decay(alpha, global_step,
+                                                decay_step, decay_rate,
+                                                staircase=True)
+    return learning_rate
