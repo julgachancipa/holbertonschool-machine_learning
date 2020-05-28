bda45497c7c79c0954dcccbdd6f07c6d7affea7a
julgachancipa <julianaga01@gmail.com>
2020-05-28 09:59:04 -0500

    [FEATURE] - Dropout Gradient Descent

---
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Gradient Descent with Dropout
+"""
+import numpy as np
+
+
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
+    """
+    updates the weights of a neural network with Dropout
+    regularization using gradient descent
+    :param Y:  one-hot mtx with the correct labels
+    :param weights:
+    :param cache:dictionary of the weights and biases of the neural network
+    :param alpha: learning rate
+    :param keep_prob: the probability that a node will be kept
+    :param L: number of layers of the network
+    :return: ntg
+    """
+    m = (Y.shape[1])
+    Al = cache["A" + str(L)]
+    dAl = Al - Y
+    for lay in reversed(range(1, L + 1)):
+        Al = cache["A" + str(lay)]
+        gl_d = 1 - np.power(Al, 2)
+        if lay == L:
+            dZl = dAl
+        else:
+            dZl = dAl * gl_d
+            dZl *= cache["D" + str(lay)]
+        Wl = weights["W" + str(lay)]
+        Al_1 = cache["A" + str(lay - 1)]
+        dWl = ((1 / m) * np.matmul(dZl, Al_1.T)) / keep_prob
+        dbl = (1 / m) * np.sum(dZl, axis=1, keepdims=True)
+        dAl = np.matmul(Wl.T, dZl)
+
+        kW = "W" + str(lay)
+        kb = "b" + str(lay)
+        weights[kW] = weights[kW] - alpha * dWl
+        weights[kb] = weights[kb] - alpha * dbl
