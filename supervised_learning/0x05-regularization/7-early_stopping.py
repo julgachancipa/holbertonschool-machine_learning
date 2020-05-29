7450aeae238bcd1224cd199a6d96a6215ad7fa7c
julgachancipa <julianaga01@gmail.com>
2020-05-29 09:36:19 -0500

    [FEATURE] - Early stopping

--- supervised_learning/0x05-regularization/7-early_stopping.py
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -6,6 +6,16 @@ import numpy as np
 
 
 def early_stopping(cost, opt_cost, threshold, patience, count):
+    """
+    determines if you should stop gradient descent early
+    :param cost: current validation cost of the neural network
+    :param opt_cost:  lowest recorded validation cost of the neural network
+    :param threshold: threshold used for early stopping
+    :param patience: patience count used for early stopping
+    :param count: count of how long the threshold has not been met
+    :return: boolean of whether the network should be stopped early,
+    followed by the updated count
+    """
     if opt_cost - cost > threshold:
         count = 0
     else:
