c9668c5fc05db13ad055aa6c21881597b49a0b76
julgachancipa <julianaga01@gmail.com>
2020-06-02 07:26:38 -0500

    [FEATURE] - Input network

---
+++ supervised_learning/0x06-keras/1-input.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Input"""
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Builds a neural network with the Keras library
+    :param nx: number of input features to the network
+    :param layers: list containing the number of nodes in
+    each layer of the network
+    :param activations:  list containing the activation
+    functions used for each layer of the network
+    :param lambtha: L2 regularization parameter
+    :param keep_prob: probability that a node will be kept for dropout
+    :return: the keras model
+    """
+    x = K.Input(shape=(nx,))
+    L = len(layers)
+    for l in range(L):
+        if l is 0:
+            y = K.layers.Dense(layers[l],
+                               kernel_regularizer=K.regularizers.l2(lambtha),
+                               activation=activations[l])(x)
+        else:
+            y = K.layers.Dense(layers[l],
+                               kernel_regularizer=K.regularizers.l2(lambtha),
+                               activation=activations[l])(y)
+        if l is not L - 1:
+            y = K.layers.Dropout(keep_prob - 1)(y)
+    return K.Model(x, y)
