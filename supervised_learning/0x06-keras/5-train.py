4a4cae11cffe3b87cdceda8dff81d27977102fd5
julgachancipa <julianaga01@gmail.com>
2020-06-02 08:17:52 -0500

    [FIX] - Validation data must be tuple

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -20,7 +20,10 @@ def train_model(network, data, labels, batch_size, epochs,
     :param validation_data: data to valid
     :return: History object generated after training the model
     """
+    if type(validation_data) is not tuple:
+        validation_data = None
     network.fit(data, labels, epochs=epochs, batch_size=batch_size,
                 verbose=verbose, shuffle=shuffle,
                 validation_data=validation_data)
+
     return network.history
