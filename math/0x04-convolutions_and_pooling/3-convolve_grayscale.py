d40ee896d3c7dd128dba6a8867ed5777e416fa29
julgachancipa <julianaga01@gmail.com>
2020-06-04 18:25:41 -0500

    [FIX] - padding function

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -14,29 +14,40 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     :param stride: tuple of (sh, sw)
     :return:
     """
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    sh = stride[0]
+    sw = stride[1]
+
     if padding == 'same':
-        images = np.pad(images, pad_width=((0, 0), (1, 1), (1, 1)),
+        oh = np.ceil(h/sh)
+        ow = np.ceil(w/sw)
+        Ph = max((oh - 1) * sh + kh, 0)
+        Pw = max((ow - 1) * sw + kw, 0)
+        pt = np.floor(Ph/2)
+        pl = np.floor(Pw/2)
+        pb = Ph - pt
+        pr = Pw - pl
+        images = np.pad(images, pad_width=((0, 0), (pt, pb), (pl, pr)),
                         mode='constant', constant_values=0)
     elif padding != 'valid':
         ph = padding[0]
         pw = padding[1]
         images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
                         mode='constant', constant_values=0)
-    m = images.shape[0]
-    ih = images.shape[1]
-    iw = images.shape[2]
-    kh = kernel.shape[0]
-    kw = kernel.shape[1]
-    hc = (ih - kh + 1)
-    wc = (iw - kw + 1)
+    hc = (h - kh + 1)
+    wc = (w - kw + 1)
 
     conv = np.zeros((m, hc // stride[0], wc // stride[1]))
 
     i = 0
-    for h in range(0, hc, stride[0]):
+    for h_i in range(0, hc, stride[0]):
         j = 0
-        for w in range(0, wc, stride[1]):
-            aux = np.multiply(images[:, h:h + kh, w:w + kw], kernel)
+        for w_i in range(0, wc, stride[1]):
+            aux = np.multiply(images[:, h_i:h_i + kh, w_i:w_i + kw], kernel)
             aux = np.reshape(aux, (m, kh * kw))
             conv[:, i, j] = np.sum(aux, axis=1)
             j += 1
