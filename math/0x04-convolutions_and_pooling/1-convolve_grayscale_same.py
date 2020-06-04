8abefea325851e04df5fd2b6f4f3859b9fddc504
julgachancipa <julianaga01@gmail.com>
2020-06-04 16:54:46 -0500

    [FIX] - padding function

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -12,10 +12,19 @@ def convolve_grayscale_same(images, kernel):
     :param kernel: kernel for the convolution
     :return: numpy.ndarray containing the convolved images
     """
-    pad_h = kernel.shape[0] // 2
-    pad_w = kernel.shape[1] // 2
-    images = np.pad(images, pad_width=((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
-                    mode='constant', constant_values=0)
+    ih = images.shape[1]
+    iw = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    hc = ih - kh + 1
+    wc = iw - kw + 1
+
+    pad_h = max((hc - 1) + kh - ih, 0)
+    pad_w = max((wc - 1) + kw - iw, 0)
+    p_t = pad_h // 2
+    p_l = pad_w // 2
+    images = np.pad(images, pad_width=((0, 0), (p_t, pad_h - p_t),
+                                       (p_l, pad_w - p_l)), mode='constant')
     m = images.shape[0]
     ih = images.shape[1]
     iw = images.shape[2]
