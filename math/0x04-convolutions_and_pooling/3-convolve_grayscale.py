12bc1660f8e3623961ba851028ef4818b1a0feb5
julgachancipa <julianaga01@gmail.com>
2020-06-04 23:43:14 -0500

    [FIX] - same case

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -26,7 +26,7 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         oh = int(np.ceil(h/sh))
         ow = int(np.ceil(w/sw))
         ph = int((((h - 1) * sh + kh - h) / 2) + 1)
-        pw = int((((h - 1) * sh + kh - h) / 2) + 1)
+        pw = int((((w - 1) * sw + kw - w) / 2) + 1)
         images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
                         mode='constant', constant_values=0)
 
