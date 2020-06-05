a21a4028b60ee8a4a26d3e9e2cff81a943de76e8
julgachancipa <julianaga01@gmail.com>
2020-06-05 13:22:17 -0500

    [FIX] - Padding

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -23,32 +23,27 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     sw = stride[1]
 
     if padding == 'same':
-        ph = int(((h - 1) * sh + kh - h) / 2) + 1
-        pw = int(((w - 1) * sw + kw - w) / 2) + 1
-        images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
-                        mode='constant', constant_values=0)
-        oh = h
-        ow = w
-
+        ph = max((h - 1) * sh + kh - h, 0)
+        pt = int(np.ceil(ph / 2))
+        pb = pt
+        pw = max((w - 1) * sw + kw - w, 0)
+        pl = int(np.ceil(pw / 2))
+        pr = pl
     elif padding == 'valid':
-        oh = int(np.ceil((h - kh + 1) / sh))
-        ow = int(np.ceil((w - kw + 1) / sw))
-
+        pt, pb, pl, pr = 0, 0, 0, 0
     else:
-        ph = padding[0]
-        pw = padding[1]
-        images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
-                        mode='constant', constant_values=0)
-        oh = ((h - kh + 2 * ph) // sh) + 1
-        ow = ((w - kw + 2 * pw) // sw) + 1
+        pt, pb = padding[0], padding[0]
+        pl, pr = padding[1], padding[1]
+
+    oh = ((h - kh + pt + pb) // sh) + 1
+    ow = ((w - kw + pl + pr) // sw) + 1
+
+    images = np.pad(images, pad_width=((0, 0), (pt, pb), (pl, pr), (0, 0)),
+                    mode='constant', constant_values=0)
 
     conv = np.zeros((m, oh, ow))
-    row = 0
     for i in range(oh):
-        col = 0
         for j in range(ow):
             aux = images[:, i * sh:i * sh + kh, j * sw:j * sw + kw] * kernel
-            conv[:, row, col] = np.sum(aux, axis=(1, 2, 3))
-            col += 1
-        row += 1
+            conv[:, i, j] = np.sum(aux, axis=(1, 2, 3))
     return conv
