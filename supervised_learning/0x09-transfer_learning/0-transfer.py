a94c697461b121d4204ae9b604827c209eb2f511
root <root@compute-instance-ide-hol-emma-gachancipa-31-24.c.maximal-grin-211714.internal>
2020-06-24 20:07:19 +0000

    [FEATURE] - use VGG16

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -16,13 +16,46 @@ def preprocess_data(X, Y):
         X_p is a numpy.ndarray containing the preprocessed X
         Y_p is a numpy.ndarray containing the preprocessed Y
     """
-    X_p = X.astype('float32')
-    X_p /= 255
+    X = X.astype('float32')
+    X_p = K.applications.vgg16.preprocess_input(X)
+    Y_p = K.utils.to_categorical(Y, 10)
 
-    Y_p= K.utils.to_categorical(Y)
-
-    return X_p, Y_p
+    return(X_p, Y_p)
 
 
-if __name__ == "__main__":
-    base_inception = K.applications.inceptionV3(weights='imagenet', include_top=False)
+(Xt, Yt), (X, Y) = K.datasets.cifar10.load_data()
+X_p, Y_p = preprocess_data(Xt, Yt)
+Xv_p, Yv_p = preprocess_data(X, Y)
+
+# print(X_p.shape, Y_p.shape)
+base_model = K.applications.vgg16.VGG16(include_top=False, weights='imagenet',
+                                        pooling='avg', classes=Y_p.shape[1])
+
+model = K.Sequential()
+model.add(K.layers.UpSampling2D())
+model.add(base_model)
+model.add(K.layers.Flatten())
+model.add(K.layers.Dense(512, activation=('relu')))
+model.add(K.layers.Dropout(0.2))
+model.add(K.layers.Dense(256, activation=('relu')))
+model.add(K.layers.Dropout(0.2))
+model.add(K.layers.Dense(10, activation=('softmax')))
+callback = []
+
+
+def decay(epoch):
+    """ This method create the alpha"""
+    return 0.001 / (1 + 1 * 30)
+
+
+callback += [K.callbacks.LearningRateScheduler(decay, verbose=1)]
+
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+history = model.fit(x=X_p, y=Y_p,
+                    batch_size=128,
+                    validation_data=(Xv_p, Yv_p),
+                    epochs=10, shuffle=True,
+                    callbacks=callback,
+                    verbose=1)
+model.save('cifar10.h5')
