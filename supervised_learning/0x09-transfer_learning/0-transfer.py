e955a343023a4e720562c511ad01aa294641e18d
julgachancipa <julianaga01@gmail.com>
2020-06-24 14:20:21 -0500

    olis

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -8,9 +8,21 @@ import tensorflow.keras as K
 def preprocess_data(X, Y):
     """
     pre-processes the data for your model
+    :param X: is a numpy.ndarray of shape (m, 32, 32, 3) containing the
+    CIFAR 10 data, where m is the number of data points
+    :param Y: Y is a numpy.ndarray of shape (m,) containing the CIFAR 10
+    labels for X
+    :return: X_p, Y_p
+        X_p is a numpy.ndarray containing the preprocessed X
+        Y_p is a numpy.ndarray containing the preprocessed Y
     """
-    X_scaled = X.astype('float32')
-    Y_scaled = Y.astype('float32')
-    X_scaled /= 255
-    Y_scaled /= 255
-    print(X.shape, Y.shape)
+    X_p = X.astype('float32')
+    X_p /= 255
+
+    Y_p= K.utils.to_categorical(Y)
+
+    return X_p, Y_p
+
+
+if __name__ == "__main__":
+    base_inception = K.applications.inceptionV3(weights='imagenet', include_top=False)
