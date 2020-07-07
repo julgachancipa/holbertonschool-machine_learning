432feb032ecfc5137e0a3d5187d6a6e9c69037f4
julgachancipa <julianaga01@gmail.com>
2020-07-06 22:14:39 -0500

    [FEATURE] - nms

--- supervised_learning/0x0A-object_detection/3-yolo.py
+++ supervised_learning/0x0A-object_detection/3-yolo.py
@@ -196,50 +196,61 @@ class Yolo():
         v_boxes = v_boxes.reshape(rows, 4)
         return v_boxes, v_labels.astype(int), v_scores
 
+    def non_max_suppression(self, filtered_boxes, box_classes, box_scores):
+        """ This method return the real boxes, scores, classes predicted """
 
-def non_max_suppression(self, filtered_boxes, box_classes, box_scores):
-    """ This method return the real boxes, scores, classes predicted """
-    if len(filtered_boxes) == 0:
-        return []
-    pick = []
+        if len(filtered_boxes) == 0:
+            return []
 
-    x1 = filtered_boxes[:, 0]
-    y1 = filtered_boxes[:, 1]
-    x2 = filtered_boxes[:, 2]
-    y2 = filtered_boxes[:, 3]
+        x1 = filtered_boxes[:, 0]
+        y1 = filtered_boxes[:, 1]
+        x2 = filtered_boxes[:, 2]
+        y2 = filtered_boxes[:, 3]
 
-    area = (x2 - x1) * (y2 - y1)
-    ind = np.lexsort((box_scores, -box_classes))
+        ind = np.lexsort((-box_scores, box_classes))
 
-    keep_idx = []
-    del_idx = []
-    classes = np.unique(box_classes)
-    for c in classes:
-        class_boxes = np.where(box_classes == c)[0]
-        idxs = np.argsort(box_scores[class_boxes])[::-1]
-        while len(idxs) > 0:
-            keep_idx.append(class_boxes[idxs[0]])
-            keep_tmp = []
-            i = idxs[0]
-            print(idxs[1:])
-            del_idx = [0]
-            aux = 0
-            for j in idxs[1:]:
-                xA = max(x1[i], x1[j])
-                yA = max(y1[i], y1[j])
-                xB = min(x2[i], x2[j])
-                yB = min(y2[i], y2[j])
+        _, class_count = np.unique(box_classes, return_counts=True)
 
-                interArea = max(0, xB - xA) * max(0, yB - yA)
-                boxAArea = (x2[i] - x1[i]) * (y2[i] - y1[i])
-                boxBArea = (x2[j] - x1[j]) * (y2[j] - y1[j])
+        i = 0
+        keep_i = []
+        # del_boxes = []
+        for c in class_count:
+            c_boxes = ind[i:i + c]
+            # print(i, c, '>>>>>>> c_boxes', c_boxes)
+            while len(c_boxes):
+                fix = c_boxes[0]
+                # print('c_b', c_boxes)
+                keep_i += [fix]
+                # print('KEEP', keep_i)
+                c_boxes = c_boxes[1:]
+                # print('c_b', c_boxes)
+                keep_tmp = []
+                for b in c_boxes:
+                    # print('________________________')
+                    # print('fix: ', filtered_boxes[fix], '-VS-',
+                    # filtered_boxes[b])
 
-                overlap = interArea / ((boxAArea + boxBArea) - interArea)
+                    xA = max(x1[fix], x1[b])
+                    yA = max(y1[fix], y1[b])
+                    xB = min(x2[fix], x2[b])
+                    yB = min(y2[fix], y2[b])
 
-                if overlap > self.nms_t:
-                    aux += 1
-                    del_idx.append(aux)
+                    interArea = max(0, xB - xA) * max(0, yB - yA)
+                    boxAArea = (x2[fix] - x1[fix]) * (y2[fix] - y1[fix])
+                    boxBArea = (x2[b] - x1[b]) * (y2[b] - y1[b])
 
-            idxs = np.delete(idxs, aux)
+                    overlap = interArea / ((boxAArea + boxBArea) - interArea)
 
-    return filtered_boxes[keep_idx], box_classes[keep_idx], box_scores[keep_idx]
+                    if overlap > self.nms_t:
+                        pass
+                        # del_boxes += [b]
+                        # print('DEL', b)
+                    else:
+                        keep_tmp += [b]
+                        # print('K_TMP', keep_tmp)
+                c_boxes = keep_tmp
+                # print('############################')
+            i += c
+
+        # ind = np.delete(ind, del_boxes, 0)
+        return filtered_boxes[keep_i], box_classes[keep_i], box_scores[keep_i]
