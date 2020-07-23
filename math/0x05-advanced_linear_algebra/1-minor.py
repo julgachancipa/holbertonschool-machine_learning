9a582e83a9779ae9074c609bdce2ea6a8a5f45ca
julgachancipa <julianaga01@gmail.com>
2020-07-23 15:08:39 -0500

    [FIX] - check all elements are list

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -59,7 +59,7 @@ def minor(matrix):
         raise TypeError('matrix must be a list of lists')
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError('matrix must be a list of lists')
-    if len(matrix) is not len(matrix[0]):
+    if matrix is [[]]:
         raise ValueError('matrix must be a non-empty square matrix')
     if len(matrix) is 1 and len(matrix[0]) is 1:
         return [[1]]
