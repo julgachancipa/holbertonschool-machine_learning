a669b2082adf0465acd0df81c2be3d39dcaebe69
julgachancipa <julianaga01@gmail.com>
2020-07-27 21:47:29 -0500

    [FIX]

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -5,23 +5,6 @@ Multivariate Normal distribution
 import numpy as np
 
 
-def mean_cov(X):
-    """
-    Calculates the mean and covariance of a data set
-    :param X: numpy.ndarray of shape (n, d) containing the data set
-    :return: mean, cov
-    """
-    if type(X) is not np.ndarray or len(X.shape) is not 2:
-        raise TypeError('X must be a 2D numpy.ndarray')
-    n = X.shape[0]
-    if n < 2:
-        raise ValueError('X must contain multiple data points')
-    mean = np.expand_dims(np.mean(X, axis=0), axis=0)
-    X -= mean
-    cov = np.dot(X.T, X) / (n - 1)
-    return mean, cov
-
-
 class MultiNormal ():
     """
     Multinormal Class
@@ -38,8 +21,19 @@ class MultiNormal ():
         n = data.shape[1]
         if n < 2:
             raise ValueError('data must contain multiple data points')
-        mean, self.cov = mean_cov(data.T)
-        self.mean = mean.T
+        self.mean_cov(data)
+
+    def mean_cov(X):
+        """
+        Calculates the mean and covariance of a data set
+        :param X: numpy.ndarray of shape (n, d) containing the data set
+        :return: mean, cov
+        """
+        d = X.shape[0]
+        n = X.shape[1]
+        self.mean = np.mean(X, axis=1).reshape(d, 1)
+        X = X - self.mean
+        self.cov = ((np.dot(X, X.T)) / (n - 1))
 
     def pdf(self, x):
         """
@@ -58,3 +52,4 @@ class MultiNormal ():
         result = (1. / (np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov))) *
                   np.exp(-(np.linalg.solve(self.cov, x_m).T.dot(x_m)) / 2))
         return result[0][0]
+
