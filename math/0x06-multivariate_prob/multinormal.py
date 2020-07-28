3fcbe75048b375f92f0d9172b816775b23e98fdf
julgachancipa <julianaga01@gmail.com>
2020-07-27 21:17:37 -0500

    [FEATURE] - multinormal pdf

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -51,7 +51,7 @@ class MultiNormal ():
         """
         if type(x) is not np.ndarray:
             raise TypeError('x must be a numpy.ndarray')
-        if x.shape is not self.mean.shape:
+        if x.shape[0] is not self.mean[0]:
             raise ValueError('x must have the shape ({d}, 1)')
         d = x.shape[0]
         x_m = x - self.mean
