9689cd1e2cec54247c98ccfb41cd6038db4c43dd
julgachancipa <julianaga01@gmail.com>
2020-07-27 21:28:21 -0500

    [FEATURE] - multinormal pdf

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -52,9 +52,9 @@ class MultiNormal ():
         if type(x) is not np.ndarray:
             raise TypeError('x must be a numpy.ndarray')
         d = self.mean.shape[0]
-        if x.shape[0] is not d:
+        if len(x.shape) != 2 or x.shape[0] is not d or x.shape[1] is not 1:
             raise ValueError('x must have the shape ({}, 1)'.format(d))
         x_m = x - self.mean
         result = (1. / (np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov))) *
                   np.exp(-(np.linalg.solve(self.cov, x_m).T.dot(x_m)) / 2))
-        return float(result)
+        return result[0][0]
