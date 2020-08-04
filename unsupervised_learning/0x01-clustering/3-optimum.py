369ec3c70699e228536f3d88f4bcdc4c23b31c1b
julgachancipa <julianaga01@gmail.com>
2020-08-04 15:28:48 -0500

    [FIX] - change order of the conditions

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -31,9 +31,9 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         kmax = X.shape[0]
     if type(kmin) != int or kmin <= 0 or kmin >= X.shape[0]:
         return None, None
-    if kmin >= kmax:
-        return None, None
     if type(kmax) != int or kmax <= 0 or kmax > X.shape[0]:
+        return None, None
+    if kmin >= kmax:
         return None, None
     results = []
     d_vars = []
