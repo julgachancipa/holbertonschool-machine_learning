1959c976290ad60ae6baaaa7872d69d171fab629
julgachancipa <julianaga01@gmail.com>
2020-08-06 16:36:31 -0500

    [FIX] - pycodestyle

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-EM
+EM: expectation maximization
 """
+import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
-import numpy as np
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
@@ -19,7 +19,16 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     likelihood, used to determine early stopping
     :param verbose: is a boolean that determines if you should print
     information about the algorithm
-    :return:
+    :return: pi, m, S, g, l, or None, None, None, None, None on failure
+        pi is a numpy.ndarray of shape (k,) containing the priors for each
+        cluster
+        m is a numpy.ndarray of shape (k, d) containing the centroid means
+        for each cluster
+        S is a numpy.ndarray of shape (k, d, d) containing the covariance
+        matrices for each cluster
+        g is a numpy.ndarray of shape (k, n) containing the probabilities
+        for each data point in each cluster
+        l is the log likelihood of the model
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return (None, None, None, None, None)
@@ -36,22 +45,23 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     prev_lkhood = 0
 
     for i in range(iterations):
-        g, l = expectation(X, pi, m, S)
+        g, lkhood = expectation(X, pi, m, S)
         pi, m, S = maximization(X, g)
 
         if verbose:
-            message = 'Log Likelihood after {} iterations: {}'.format(i, l)
+            message = 'Log Likelihood after {} iterations: {}'.format(i,
+                                                                      lkhood)
             if (i % 10 == 0) or (i == 0):
                 print(message)
             if i == (iterations - 1):
                 print(message)
 
-            if abs(l - prev_lkhood) <= tol:
+            if abs(lkhood - prev_lkhood) <= tol:
                 print(message)
                 break
             if i == (iterations + 1):
                 break
-        if abs(l - prev_lkhood) <= tol:
+        if abs(lkhood - prev_lkhood) <= tol:
             break
-        prev_lkhood = l
-    return pi, m, S, g, l
+        prev_lkhood = lkhood
+    return pi, m, S, g, lkhood
