0a90639c5e209b91a3ef397732dfad7812b389fe
julgachancipa <julianaga01@gmail.com>
2020-08-06 16:42:13 -0500

    [FIX] - tol == 0.

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -36,7 +36,7 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         return (None, None, None, None, None)
     if type(iterations) != int or iterations <= 0:
         return (None, None, None, None, None)
-    if type(tol) != float or tol <= 0:
+    if type(tol) != float or tol < 0:
         return (None, None, None, None, None)
     if type(verbose) != bool:
         return None, None, None, None, None
