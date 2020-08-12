dff4a28e123d9e97e167d93867377e3f4c092c5e
julgachancipa <julianaga01@gmail.com>
2020-08-12 13:17:28 -0500

    [FEATURE] - Regular

---
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Regular Chains
+"""
+import numpy as np
+
+
+def regular(P):
+    """
+    Determines the steady state probabilities of a regular markov chain
+    :param P: is a is a square 2D numpy.ndarray of shape (n, n) representing
+    the transition matrix
+        P[i, j] is the probability of transitioning from state i to state j
+        n is the number of states in the markov chain
+    :return: a numpy.ndarray of shape (1, n) containing the steady state
+    probabilities, or None on failure
+    """
+    try:
+        cols = P.shape[0]
+        ans = np.ones((1, cols))
+        eq = np.vstack([P.T - np.identity(cols), ans])
+        results = np.zeros((cols, 1))
+        results = np.vstack([results, np.array([1])])
+        stationary = np.linalg.solve(eq.T.dot(eq), eq.T.dot(results)).T
+        if len(np.argwhere(stationary < 0)) > 0:
+            return None
+        return stationary
+    except Exception:
+        return None
