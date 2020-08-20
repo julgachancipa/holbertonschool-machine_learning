83b57bc44d0e0c50e1a455efaab87b50812e068e
julgachancipa <julianaga01@gmail.com>
2020-08-20 09:28:25 -0500

    [UPDATE] - documentation

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -6,6 +6,9 @@ import numpy as np
 
 
 class GaussianProcess:
+    """
+    Represents a noiseless 1D Gaussian process
+    """
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """
         Class constructor
