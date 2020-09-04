5eae0b2497e63d0b5352e2977415df2ef5e8b789
julgachancipa <julianaga01@gmail.com>
2020-09-04 15:20:20 -0500

    [FEATURE] - bi backward

--- supervised_learning/0x0D-RNNs/6-main.py
+++ supervised_learning/0x0D-RNNs/6-main.py
@@ -4,7 +4,7 @@ import numpy as np
 BidirectionalCell = __import__('6-bi_backward'). BidirectionalCell
 
 np.random.seed(6)
-bi_cell =  BidirectionalCell(10, 15, 5)
+bi_cell = BidirectionalCell(10, 15, 5)
 bi_cell.bhb = np.random.randn(1, 15)
 h_next = np.random.randn(8, 15)
 x_t = np.random.randn(8, 10)
