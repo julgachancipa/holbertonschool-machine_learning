e095c0203b7ef31b730708e560284d2e36f1f07c
julgachancipa <julianaga01@gmail.com>
2020-09-04 19:02:50 -0500

    [FIX] - l1 reg

--- unsupervised_learning/0x04-autoencoders/1-sparse.py
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -27,9 +27,9 @@ def sparse(input_dims, hidden_layers, latent_dims, lambtha):
 
     l_1 = keras.regularizers.l1(lambtha)
     for n in hidden_layers:
-        encoded = keras.layers.Dense(n, activation='relu',
-                                     kernel_regularizer=l_1)(input_encoded)
+        encoded = keras.layers.Dense(n, activation='relu')(input_encoded)
         input_encoded = encoded
+
     latent = keras.layers.Dense(latent_dims, activation='relu',
                                 kernel_regularizer=l_1)(encoded)
     encoder = keras.models.Model(input_encoder, latent)
