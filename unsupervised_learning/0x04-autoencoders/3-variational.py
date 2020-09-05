dc4bf07cff8c25eb8d244b897f7e020aba806220
julgachancipa <julianaga01@gmail.com>
2020-09-04 19:27:01 -0500

    [FEATURE] - tmp

--- unsupervised_learning/0x04-autoencoders/3-variational.py
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -21,4 +21,27 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         -decoder is the decoder model
         -auto is the full autoencoder model
     """
-    pass
+    input_encoder = keras.layers.Input(shape=(input_dims,))
+    input_encoded = input_encoder
+    for n in hidden_layers:
+        encoded = keras.layers.Dense(n, activation='relu')(input_encoded)
+        input_encoded = encoded
+    latent = keras.layers.Dense(latent_dims, activation='relu')(encoded)
+    encoder = keras.models.Model(input_encoder, latent)
+
+    input_decoder = keras.layers.Input(shape=(latent_dims,))
+    input_decoded = input_decoder
+    for i, n in enumerate(hidden_layers[::-1]):
+        decoded = keras.layers.Dense(n, activation='relu')(input_decoded)
+        input_decoded = decoded
+    decoded = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
+    decoder = keras.models.Model(input_decoder, decoded)
+
+    input_auto = keras.layers.Input(shape=(input_dims,))
+    encoderOut = encoder(input_auto)
+    decoderOut = decoder(encoderOut)
+    auto = keras.models.Model(input_auto, decoderOut)
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
+
